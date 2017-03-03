d76f98870f3c6e8d49a8814e6dac76a08d529df9
es47 <kl8534ghj4633@gmail.com>
2017-03-03 15:35:57 +0800

    Add files via upload

---
+++ 104-1/2015-10-29/404410062_bubblesort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+main()
+{
+    int n, m[200];
+    int i, j, temp, t;
+    while (1)
+    {
+        scanf("%d", &n);
+        if (n == 0) break;
+        for (i = 0; i < n; i++)
+            scanf(" %d", &(m[i]));
+        t = 0;
+        for (i = n - 1; i > 0; i--)
+        {
+            for (j = n - 1; j > 0; j--)
+            {
+                if (m[j] < m[j - 1])
+                {
+                    temp = m[j];
+                    m[j] = m[j - 1];
+                    m[j - 1] = temp;
+                    t++;
+                }
+            }
+        }
+        printf("%d\n", t);
+    }
+}
---
+++ 104-1/2015-10-29/404410062_selectsort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+main()
+{
+    int n, m[200];
+    int i, j, min, t, temp, x;
+    while (1)
+    {
+        scanf("%d", &n);
+        if (n == 0) break;
+        for (i = 0; i < n; i++)
+            scanf(" %d", &m[i]);
+        t = 0;
+        for (i = 0; i < n; i++)
+        {
+            min = m[i];
+            for (j = i + 1; j < n; j++)
+            {
+                if (min < m[j])
+                    min = min;
+                else
+                    min = m[j];
+                if (min == m[j])
+                    x = j;
+            }
+            if (min != m[i])
+            {
+                temp = m[i];
+                m[i] = min;
+                m[x] = temp;
+                t++;
+            }
+        }
+        if (n < 3)
+        {
+            for (i = 0; i < n; i++)
+                printf("%d ", m[i]);
+            printf("%d\n", t);
+        }
+        else
+            printf("%d %d %d %d\n", m[0], m[1], m[2], t);
+    }
+}
